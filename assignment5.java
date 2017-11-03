d094ed3dae7f618901a78eca4f4c66d966f3b749
l4cuatro <lpieroni@sfhighschool.net>
2017-11-02 18:45:13 -0700

    Update assignment5.java

--- assignment5.java
+++ assignment5.java
@@ -1,6 +1,5 @@
   import java.util.Scanner;
   public class Assignment_5 {
-    public static String [] list = {};
     public static void main (String[]args) {
     
       Scanner scan = new Scanner (System.in);
