84ad626493e3435841030d2e480db55676f383db
l4cuatro <lpieroni@sfhighschool.net>
2017-11-02 19:01:20 -0700

    Corrected a few things, also renamed the class to assignment5

--- assignment5.java
+++ assignment5.java
@@ -1,5 +1,5 @@
   import java.util.Scanner;
-  public class Assignment_5 {
+  public class assignment5 {
     public static void main (String[]args) {
     
       Scanner scan = new Scanner (System.in);
