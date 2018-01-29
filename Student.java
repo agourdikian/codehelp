9bd2bc985998f94109a357481feceed4728361c9
agourdikian <agourdikian@sfhighschool.net>
2018-01-28 21:18:12 -0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,45 @@
+ class Student {
+  
+  private String first = "", //if you add a comma you don't need to repeat 'private string'
+  last = "";
+  
+  private int grade,
+  id;
+  
+  private double gpa; 
+  
+  private static int idlast = 0;
+  
+  Student(String firstName, String lastName, int gradeLevel, double GPA){
+    first = firstName;
+    last = lastName;
+    grade = gradeLevel;
+    gpa = GPA;
+    id = ++idlast;
+  }
+   
+  Student(){
+    "None", "None", 0, 0.0;
+  }
+
+  String  toString()
+  { 
+    return(last + ", " + first + "\nGPA: " + gpa + "\nGrade Level: " + grade + "id #" + id ());
+  }
+  
+  if (gradeLevel >= 0 &&  gradeLevel <= 12){
+    grade = gradeLevel
+  }
+  else {
+    grade = 0
+  }
+  
+  if (gpa >= 0.0 && gpa <= 4.5) {
+    gpa = GPA;
+  }
+  else {
+      gpa = 0.0
+  }
+  
+} 
+ 
