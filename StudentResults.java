04c5105f6adc22cb14910c7fb77e8e9d145aae67
agourdikian <agourdikian@sfhighschool.net>
2018-01-30 21:56:25 -0800

    Create StudentResults.java

---
+++ StudentResults.java
@@ -0,0 +1,14 @@
+public class StudentResults {
+  
+  private String Full_name;
+  private double Exam_grade;
+  
+  StudentResults (String name, double grade){
+    
+    Full_name = name;
+    Exam_grade = grade;
+  }
+
+}
+
+//How do i print this??
