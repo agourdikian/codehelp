6a7cf1e73623dbcb8d35526e7ed2129a66d2d482
agourdikian <agourdikian@sfhighschool.net>
2018-01-30 22:08:14 -0800

    Create StudentResults.java

---
+++ StudentResults.java
@@ -0,0 +1,15 @@
+public class StudentResults {
+  
+  private String Full_name;
+  private double Exam_grade;
+  
+ public StudentResults (String Full_name, double Exam_grade){
+
+   
+    Full_name = "Alex Gourdikian";
+    Exam_grade = 95.2;
+    
+    StudentResults student = new StudentResults
+  }
+
+}
